8a98fdb311c99c37358b946144b0c4f7a2349b64
tachibana-marine <d.ageha.sekaijyu1041@gmail.com>
2024-10-06 00:27:32 +0900

    fix the bug ("Try again" crashes the game after second try)

--- run_right.gd
+++ run_right.gd
@@ -5,32 +5,35 @@ extends Node
 @onready var start_game_scene = load("res://start_game.tscn")
 @onready var main_scene = load("res://main.tscn")
 @onready var game_over_scene = load("res://asset/game_over.tscn")
+var start_game: StartGame = null
+var main: Main = null
+var game_over: GameOver = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var start_game = start_game_scene.instantiate()
+	start_game = start_game_scene.instantiate()
 	start_game.name = "StartGame"
 	start_game.connect("game_start", _on_game_start)
 	add_child(start_game)
 
 func _start_game():
-	var main = main_scene.instantiate()
+	main = main_scene.instantiate()
 	main.name = "Main"
 	main.connect("player_died", _on_player_died)
 	add_child(main)
 
 func _on_game_start():
-	$StartGame.queue_free()
+	start_game.queue_free()
 	_start_game()
 
 func _on_player_died(score: int):
-	var game_over = game_over_scene.instantiate()
+	game_over = game_over_scene.instantiate()
 	game_over.name = "GameOver"
 	game_over.score = score
 	add_child(game_over)
 	game_over.connect("try_again", _on_try_again)
 
 func _on_try_again():
-	$Main.queue_free()
-	$GameOver.queue_free()
+	main.queue_free()
+	game_over.queue_free()
 	_start_game()
